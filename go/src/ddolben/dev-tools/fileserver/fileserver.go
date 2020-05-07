8d38dda844e7f7298c271b9f172457bd8608f27e
Dave Dolben <driver13+work@gmail.com>
2020-05-07 11:14:18 -0700

    Added directory mapping to fileserver

--- go/src/ddolben/dev-tools/fileserver/fileserver.go
+++ go/src/ddolben/dev-tools/fileserver/fileserver.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "log"
   "net/http"
+  "strings"
 )
 
 type HTTPLogger struct {
@@ -16,12 +17,52 @@ func (hl *HTTPLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   hl.h.ServeHTTP(w, r)
 }
 
+type DirMapping struct {
+  URLPrefix string
+  ActualDir string
+}
+
+type MappedDirServer struct {
+  // Custom mappings from URL dir to actual dir.
+  Mappings []DirMapping
+  StaticServer http.Handler
+}
+
+// If the URL has a mapped dir associated with it, serve that. Otherwise, just do a normal fileserve operation.
+func (sv *MappedDirServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  path := r.URL.Path
+  for _, m := range sv.Mappings {
+    if strings.HasPrefix(path, m.URLPrefix) {
+      http.StripPrefix(m.URLPrefix, http.FileServer(http.Dir(m.ActualDir))).ServeHTTP(w, r)
+      return
+    }
+  }
+  sv.StaticServer.ServeHTTP(w, r)
+}
+
 func main() {
   fDir := flag.String("dir", ".", "Directory to serve")
   fPort := flag.Int("port", 8080, "Port on which to serve")
   flag.Parse()
 
-  http.Handle("/", &HTTPLogger{ h: http.FileServer(http.Dir(*fDir)) })
+  var mappings []DirMapping
+  for _, pair := range flag.Args() {
+    s := strings.Split(pair, ":")
+    if len(s) != 2 {
+      log.Fatal("bad mapping: %q", pair)
+    }
+    mappings = append(mappings, DirMapping{
+      URLPrefix: s[0],
+      ActualDir: s[1],
+    })
+  }
+
+  sv := &MappedDirServer{
+    Mappings: mappings,
+    StaticServer: http.FileServer(http.Dir(*fDir)),
+  }
+
+  http.Handle("/", &HTTPLogger{ h: sv })
 
   host := fmt.Sprintf(":%d", *fPort)
   log.Printf("serving on %s", host)
