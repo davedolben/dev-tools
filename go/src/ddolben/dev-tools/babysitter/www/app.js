dea525eadf6d4ae90360758118ff3f064ccd2aea
Dave Dolben <driver13+work@gmail.com>
2020-05-07 15:57:43 -0700

    Some web improvements to babysitter

--- go/src/ddolben/dev-tools/babysitter/www/app.js
+++ go/src/ddolben/dev-tools/babysitter/www/app.js
@@ -1,12 +1,72 @@
 (function() {
 
+class TaskTracker {
+  constructor(rootElem, name) {
+    this.rootElem = rootElem;
+    this.name = name;
+
+    this.rootElem.classList.add("task");
+
+    let nameElem = document.createElement("span");
+    nameElem.classList.add("task-name")
+    nameElem.innerHTML = name;
+    this.rootElem.appendChild(nameElem);
+    
+    this.statusElem = document.createElement("span");
+    this.statusElem.classList.add("task-status");
+    this.rootElem.appendChild(this.statusElem);
+
+    this.update("running");
+  }
+
+  update(status) {
+    if (status === "running") {
+      this.rootElem.classList.add("task-running");
+      this.rootElem.classList.remove("task-complete");
+    }
+    if (status === "complete") {
+      this.rootElem.classList.remove("task-running");
+      this.rootElem.classList.add("task-complete");
+    }
+    this.statusElem.innerHTML = status;
+  }
+}
+
 let appRoot = document.querySelector("#container");
+let tasksRoot = appRoot.querySelector("#tasks");
 let logRoot = appRoot.querySelector("#log");
+
+let tasks = {};
+
+function addTask(key) {
+  if (key in tasks) {
+    tasks[key].update("running");
+  } else {
+    let newElem = document.createElement("div");
+    tasksRoot.prepend(newElem);
+    tasks[key] = new TaskTracker(newElem, key);
+  }
+}
+
+function completeTask(key) {
+  if (!(key in tasks)) {
+    console.error("Tried to end task that wasn't started:", key);
+    return;
+  }
+  tasks[key].update("complete");
+}
 
 function onSignal(data) {
   let newDiv = document.createElement("div");
   newDiv.innerHTML = "[" + data.type + "] " + data.key;
-  logRoot.appendChild(newDiv);
+  logRoot.prepend(newDiv);
+
+  if (data.type === "start") {
+    addTask(data.key);
+  }
+  if (data.type === "end") {
+    completeTask(data.key);
+  }
 }
 
 function connect() {
