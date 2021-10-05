56b1b051e6e486e2d02630d997ca567aa1eec786
Dave Dolben <ddolben@lyft.com>
2021-10-05 18:25:01 +0000

    Fix babysitter.sh

--- bin/babysitter.sh
+++ bin/babysitter.sh
@@ -3,9 +3,9 @@
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
 ${script_dir}/../go-bin/babysitter \
-  --static_dir=${script_dir}/../go/src/ddolben/dev-tools/babysitter/www \
+  --static_dir=${script_dir}/../go/babysitter/www \
   --use_ssl \
-  --ssl_cert=${script_dir}/../ssl/Certificate.crt \
-  --ssl_key=${script_dir}/../ssl/Key.key \
+  --ssl_cert=${script_dir}/../ssl/generated/Certificate.crt \
+  --ssl_key=${script_dir}/../ssl/generated/Key.key \
   "$@"
 
