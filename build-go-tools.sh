ee6af48757346099ce1098e327aa34aa11029b92
Dave Dolben <driver13@gmail.com>
2022-02-16 22:36:29 +0000

    Add macos cross compile option to build-go-tools.sh

--- build-go-tools.sh
+++ build-go-tools.sh
@@ -8,11 +8,31 @@ mkdir -p "$bin_dir"
 
 cd "${script_dir}/go"
 
+DO_MAC=
+while [ "$#" -gt "0" ]; do
+  case "$1" in
+    "--mac")
+      DO_MAC=true
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 build() {
   cd "$1"
   go build -o "${bin_dir}/$2"
   cd -
 }
+
+if [ ! -z "$DO_MAC" ]; then
+  echo "Cross-compiling for macos"
+  export GOOS=darwin
+  export GOARCH=amd64
+  bin_dir="${bin_dir}/macos"
+fi
 
 build fileserver fileserver
 build babysitter babysitter
