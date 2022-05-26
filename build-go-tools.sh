f35cd48d8f9469f541365afa007fdd54d60ab3b0
Dave Dolben <driver13@gmail.com>
2022-05-26 12:57:46 -0700

    Fixed filedrop build

--- build-go-tools.sh
+++ build-go-tools.sh
@@ -8,15 +8,22 @@ mkdir -p "$bin_dir"
 
 cd "${script_dir}/go"
 
+# Add these as environment variables to turn on features
 DO_MAC=
+DO_ARM=
 while [ "$#" -gt "0" ]; do
   case "$1" in
-    "--mac")
+    "--macos")
       DO_MAC=true
       shift
       ;;
+    "--arm")
+      DO_ARM=true
+      shift
+      ;;
     *)
-      break
+      echo "Unrecognized argument: $1"
+      exit 1
       ;;
   esac
 done
@@ -31,7 +38,16 @@ if [ ! -z "$DO_MAC" ]; then
   echo "Cross-compiling for macos"
   export GOOS=darwin
   export GOARCH=amd64
-  bin_dir="${bin_dir}/macos"
+fi
+
+# Do this next to make sure it overrides the previous value
+if [ ! -z "$DO_ARM" ]; then
+  echo "Cross-compiling for arm"
+  export GOARCH=amd64
+fi
+
+if [ ! -z "$GOOS" ]; then
+  bin_dir="${bin_dir}/${GOOS}-${GOARCH}"
 fi
 
 build fileserver fileserver
@@ -39,3 +55,14 @@ build babysitter babysitter
 build notes notes-server
 build captains_chair captains-chair
 
+(
+  # Clear the environment variables (just in this subshell) so we can execute the codegen step
+  # on the local system regardless of the target architecture we're compiling to.
+  export GOOS=
+  export GOARCH=
+  cd filedrop
+  ./codegen.sh
+  cd -
+)
+
+build filedrop filedrop
