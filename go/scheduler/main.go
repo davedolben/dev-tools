e5ff88bdd763dca4121a6a5a09608087cb9a78b5
Dave Dolben <davedolben@hightouch.io>
2024-02-29 17:18:18 -0800

    Scheduler in its own file

--- go/scheduler/main.go
+++ go/scheduler/main.go
@@ -3,56 +3,7 @@ package main
 import (
   "fmt"
   "time"
-  "log"
 )
-
-type ITask interface {
-  Run() error
-}
-
-type ISchedule interface {
-  IsDue(lastRun time.Time, evalTimestamp time.Time) bool
-  ToString() string
-}
-
-type ScheduledTask struct {
-  Name string
-  Schedule ISchedule
-  Task ITask
-}
-
-type Scheduler struct {
-  Tasks []*ScheduledTask
-}
-
-func (s *Scheduler) Start() {
-  for _, task := range s.Tasks {
-    log.Printf("Registering task %q with schedule: %s", task.Name, task.Schedule.ToString())
-  }
-
-  // TODO: replace
-  getTaskId := func (t *ScheduledTask) string {
-    return fmt.Sprintf("%p", t)
-  }
-  lastRuntimes := make(map[string]time.Time)
-
-  for {
-    now := time.Now()
-    for _, task := range s.Tasks {
-      id := getTaskId(task)
-      lastRuntime, found := lastRuntimes[id]
-      if !found || task.Schedule.IsDue(lastRuntime, now) {
-        log.Printf("Running task %q", task.Name)
-        task.Task.Run()
-        log.Printf("Finished running task %q", task.Name)
-        lastRuntimes[id] = now
-      }
-    }
-    time.Sleep(time.Second)
-  }
-}
-
-//////////
 
 type TestTask struct {
   Name string
