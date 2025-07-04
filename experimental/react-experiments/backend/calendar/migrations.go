0ec39c81cd030ee1993d9f9ab992a09770409d52
Dave Dolben <driver13@gmail.com>
2025-07-03 21:58:29 -0700

    Move migration harness into util

--- experimental/react-experiments/backend/calendar/migrations.go
+++ experimental/react-experiments/backend/calendar/migrations.go
@@ -2,17 +2,11 @@ package calendar
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
+
+	"github.com/davedolben/dev-tools/experimental/react-experiments/sqlutil"
 )
 
-type Migration struct {
-	Version int
-	Up      func(db *sql.DB) error
-	// TODO: Down
-}
-
-var migrations = []Migration{
+var migrations = []sqlutil.Migration{
 	{
 		Version: 1,
 		Up: func(db *sql.DB) error {
@@ -46,48 +40,5 @@ var migrations = []Migration{
 }
 
 func migrateDB(db *sql.DB) error {
-	// Add a migrations table
-	_, err := db.Exec(`
-		CREATE TABLE IF NOT EXISTS migrations (
-			version INTEGER PRIMARY KEY NOT NULL,
-			applied_at INTEGER
-		)`)
-	if err != nil {
-		return fmt.Errorf("error creating migrations table: %v", err)
-	}
-
-	// Get the highest applied migration version
-	var highestAppliedVersion *int
-	err = db.QueryRow(`SELECT MAX(version) FROM migrations`).Scan(&highestAppliedVersion)
-	if err != nil {
-		return fmt.Errorf("error getting highest applied migration version: %v", err)
-	}
-	if highestAppliedVersion == nil {
-		highestAppliedVersion = new(int)
-		*highestAppliedVersion = 0
-	}
-
-	log.Printf("Highest applied migration version: %d", *highestAppliedVersion)
-
-	for _, migration := range migrations {
-		// Check if migration has already been run
-		if migration.Version <= *highestAppliedVersion {
-			continue
-		}
-
-		err = migration.Up(db)
-		if err != nil {
-			return fmt.Errorf("error running migration %d: %v", migration.Version, err)
-		}
-
-		// Update the highest applied migration version
-		_, err = db.Exec(`INSERT INTO migrations (version, applied_at) VALUES (?, UNIXEPOCH())`, migration.Version)
-		if err != nil {
-			return fmt.Errorf("error updating highest applied migration version: %v", err)
-		}
-
-		log.Printf("Applied migration %d", migration.Version)
-	}
-
-	return nil
+	return sqlutil.MigrateDB(db, migrations)
 }
